47abfebaa5e86d379a3f0f252a62d3f13b525376
Bobby Rathore <bobbyprathore@gmail.com>
2018-12-21 19:14:10 -0530

    [ADD] sentence serializers and bug fixes.

--- tests/answers_scorer.py
+++ tests/answers_scorer.py
@@ -16,9 +16,7 @@ tests = [
     ['You', 0],
     ['What \' s your', 0],
 ]
-
 init()
-
 for test in tests:
     scored = score_answers([test[0]])
     print('[{}]  {}  ->  {}{}'.format(Fore.GREEN + 'PASS' + Fore.RESET if scored[0] == test[1] else Fore.RED + 'FAIL' + Fore.RESET, test[0], test[1], '' if scored[0] == test[1] else '  Result: {}'.format(scored[0])))
